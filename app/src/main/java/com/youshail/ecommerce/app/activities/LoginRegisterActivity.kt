39acfb1b2fd0e07d5d72ac1c2e88caa252c79428
YOUSSEF SOUHAIL <s.youssef91@gmail.com>
2023-09-07 16:50:51 +0100

    Initial commit
    setup project structure and development

---
+++ app/src/main/java/com/youshail/ecommerce/app/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.youshail.ecommerce.app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.youshail.ecommerce.app.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
---
+++ app/src/main/java/com/youshail/ecommerce/app/activities/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.youshail.ecommerce.app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.youshail.ecommerce.app.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
