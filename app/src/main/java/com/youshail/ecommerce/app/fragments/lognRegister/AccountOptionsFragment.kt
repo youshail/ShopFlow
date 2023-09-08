e3946aea5191ee5a4c365d10bc205c37dad6399c
YOUSSEF SOUHAIL <s.youssef91@gmail.com>
2023-09-08 11:43:18 +0100

    Introduction Fragment

---
+++ app/src/main/java/com/youshail/ecommerce/app/fragments/lognRegister/AccountOptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.youshail.ecommerce.app.fragments.lognRegister
+
+import androidx.fragment.app.Fragment
+import com.youshail.ecommerce.app.R
+
+class AccountOptionsFragment: Fragment(R.layout.fragment_acount_options) {
+}
